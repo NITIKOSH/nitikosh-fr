fff4987a11dcab2bdff87fdf2980eda019d8739b
ANSH GOYAL <72507723+Ansh1693@users.noreply.github.com>
2023-09-26 22:46:53 +0530

    feat: button tsx

--- src/components/ui/buttons/PrimaryButton.tsx
+++ src/components/ui/buttons/PrimaryButton.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const PrimaryButton = ({ children, classes, ...props }) => {
+type x = {
+	children: any
+	onClick: any
+	classes: any
+	props: any
+}
+
+const PrimaryButton: React.FC<x> = ({
+	children,
+	onClick,
+	classes,
+	...props
+}) => {
 	return (
 		<div
 			className={` h-full w-full flex items-center justify-center cursor-pointer text-white bg-gradient-to-r stroke-white from-primary-blue-dark to-primary-blue-light ${classes}`}
 			onClick={() => {
-				if (!props.onClick) return
-				props.onClick()
+				if (!onClick) return
+				onClick()
 			}}
 			{...props}
 		>
--- src/components/ui/buttons/SecondaryButton.tsx
+++ src/components/ui/buttons/SecondaryButton.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
-const SecondaryButton = ({ children, onClick, classes, ...props }) => {
+type x = {
+	children: any
+	onClick: any
+	classes: any
+	props: any
+}
+
+const SecondaryButton: React.FC<x> = ({
+	children,
+	onClick,
+	classes,
+	...props
+}) => {
 	return (
 		<div
 			className={` h-full w-full flex items-center justify-center cursor-pointer text-primary bg-white stroke-primary border border-primary ${classes}`}
