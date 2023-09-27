7bc189631ef4584acd80e183733dd88667f189d3
Sneha Virmani <snehavir13@gmail.com>
2023-09-27 15:52:56 +0530

    dashboard navbar

--- src/components/Forms/Nav.tsx
+++ src/components/Forms/Nav.tsx
@@ -4,7 +4,38 @@ type x = {
 	steps: any
 }
 const Nav: React.FC<x> = ({ steps }) => {
-	return <div className=' h-20 w-full'></div>
+	return <div className=' h-20 w-full border-b-[1px] border-[#211D3D80]'>
+		<div className='flex flex-row'>
+			<div className='p-5 w-1/4 flex flex-row'>
+				<div className='py-3 pl-5'>
+					<div className='w-6 h-6 border-2 border-purple rounded-full '></div>
+				</div>
+				<div className='py-3 pl-3 pr-5'>Case Information</div>
+			</div>
+			<div className='p-5 w-1/4 flex flex-row'>
+				<div className='py-3 pl-5'>
+					<div className='w-6 h-6 border-2 border-purple rounded-full '></div>
+				</div>
+				<div className='py-3 pl-3 pr-5'>Case Documentation</div>
+			</div>
+			<div className='p-5 w-1/4 flex flex-row'>
+				<div className='py-3 pl-5'>
+					<div className='w-6 h-6 border-2 border-purple rounded-full '></div>
+				</div>
+				<div className='py-3 pl-3 pr-5'>Case Details</div>
+			</div>
+			<div className='p-5 w-1/4 flex flex-row'>
+				<div className='py-3 pl-5'>
+					<div className='w-6 h-6 border-2 border-purple rounded-full '></div>
+				</div>
+				<div className='py-3 pl-3 pr-5'>Review & Submit</div>
+			</div>
+			
+		</div>
+		<div className='hidden'></div>
+		
+		
+	</div>
 }
 
 export default Nav
